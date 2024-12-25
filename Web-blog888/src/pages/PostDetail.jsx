aa6370aeb94d7f8625b32f9b1203f31bb9668312
Punsano26 <xlichaphlreuxn@gmail.com>
2024-12-25 11:34:31 +0700

    Post Detail Demo

--- Web-blog888/src/pages/PostDetail.jsx
+++ Web-blog888/src/pages/PostDetail.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import PostService from "../services/post.service";
 import Swal from "sweetalert2";
 import { useAuthContext } from "../context/AuthContext";
+import { useParams } from "react-router";
 const Post = () => {
   const [postDeatail, setPostsDetail] = useState(null)
   const {id} = useParams();
@@ -9,6 +10,9 @@ const Post = () => {
     const fetchPost = async () => {
       try {
      const response = await PostService.getPostByID();
+      if (response.status === 200) {
+        setPostsDetail(response.data);
+      }
     } catch (error) {
        Swal.fire({
         title: "Post Detail",
@@ -20,7 +24,33 @@ const Post = () => {
     fetchPost();
   },[id])
   
-  return <div>is Athor {postDeatail?.author?._id === user?.id && <p>true</p>}</div>;
+  return (
+    <>
+    <div className="container mx-auto max-w-3xl mt-20 px-4">  
+      <div className="card card-side bg-base-100 shadow-xl rounded-lg">
+        {postDeatail && (
+          <>
+          <figure className="md:1/2 flex items-center justify-center">
+            <img
+              src={`${baseURL}/${postDeatail.cover}`}
+              alt={postDeatail.title}
+              className="w-full h-64 object-cover"
+            />
+          </figure>
+          <div className="p-6 md:1/2 flex flex-col justify-between card-body">
+            <h2 className="card-title">{postDeatail.title}</h2>
+            <p>
+              {postDeatail.author.username}-{postDeatail.createdAt}
+            </p>
+            <p>{postDeatail.summary}</p>
+            <p>{postDeatail.content}</p>
+          </div>
+          </>
+        )}
+      </div>
+    </div>
+    </>
+  )
 };
 
 export default Post;
