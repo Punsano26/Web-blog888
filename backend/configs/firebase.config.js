a7ead598a829d4d980db856a4a37b4f59df64522
punsano26 <xlichaphlreuxn@gmail.com>
2025-01-02 11:56:04 +0700

    config tailwind

---
+++ backend/configs/firebase.config.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyATuoNPrYe4ZHX99kWxiCt1M0PnWVJImdE",
+    authDomain: "component-b8f46.firebaseapp.com",
+    projectId: "component-b8f46",
+    storageBucket: "component-b8f46.firebasestorage.app",
+    messagingSenderId: "21962389027",
+    appId: "1:21962389027:web:7085072a3084615fe9a396"
+};
+
+module.exports = firebaseConfig;
