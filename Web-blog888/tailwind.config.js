455283a790636f250ee6f899f4e60b922dc218ab
Punsano26 <xlichaphlreuxn@gmail.com>
2025-01-03 11:33:12 +0700

    Debug Deploy

--- Web-blog888/tailwind.config.js
+++ Web-blog888/tailwind.config.js
@@ -1,10 +1,18 @@
+// tailwind.config.js
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
-};
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
